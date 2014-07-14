87636f2222d18858c341782f9dcac3cfdc3c9dfe
Courtney Campany <courtneycampany@gmail.com>
2014-07-14 16:26:59 +1000

    deleted 3d plotting and old model setups, made a new script from shuns model setup

--- Stats/photochem_reg.R
+++ Stats/photochem_reg.R
@@ -8,7 +8,7 @@ photo_chem <- read.csv("calculated data/Amax_chem.csv")
   photo_chem<- vollab_func(photo_chem)
 
 #--------------------------------------------------------------------------------------
-#calculate overall means and means for each date
+#calculate overall means
 
 #means(all dates)
 photo_chem_agg <- summaryBy(A_mass+starch+Nmass_notnc ~ volume, FUN=c(mean, se), 
@@ -17,9 +17,6 @@ photo_chem_agg <- summaryBy(A_mass+starch+Nmass_notnc ~ volume, FUN=c(mean, se),
   volumeorder <- order(photo_chem_agg$A_mass.mean, by=photo_chem_agg$volume)
   photo_chem_agg<-photo_chem_agg[volumeorder,]
 
-# #means(campaign)
-# photo_chem_campaign <- summaryBy(A_mass+starch+Nmass_notnc ~ campaign + volume, FUN=c(mean, se), 
-#                                   keep.names=TRUE, data=photo_chem)
 
 #simple model mass based--------------------------------------------------------------------
 
@@ -29,75 +26,6 @@ Amass_simple <- lm(A_mass~ starch*Nmass_notnc, data=photo_chem)
   summary(Amass_simple)
   visreg(Amass_simple)
 
-AmassN <- lm(A_mass~ Nmass_notnc, data=photo_chem)
-  anova(AmassN)
-  summary(AmassN)
-  visreg(AmassN)
-
-AmassN2 <- lm(A_mass~ Nmass_notnc*volume, data=photo_chem)
-  visreg(AmassN2, xvar="Nmass_notnc", by="volume", overlay=TRUE)
-
-AmassS <- lm(A_mass~ starch, data=photo_chem)
-  anova(AmassS)
-  summary(AmassS)
-  visreg(AmassS)
-
-AmassS2 <- lm(A_mass~ starch*volume, data=photo_chem)
-  visreg(AmassS2, xvar="starch", by="volume", overlay=TRUE)
-
-#visualise effects in 3d--------------------------------------------------------------------------
-
-windows()
-with(photo_chem, {
-  scatterplot3d(Nmass_notnc,   # x axis
-                starch,     # y axis
-                A_mass,    # z axis
-                scale.y=.65, #reduces yaxis by 25%
-                type="h", lty.hplot=2, pch=pchs[volume], color=volume,
-                main="Photosynthesis vs N and TNC",xlab=nfree, ylab=starchlab, zlab=Amasslab)
-                legend("right", leglab, inset=.1,     
-                       bty="n", text.font=2,pch=pchs, col=palette(), title=vollab)
-             
-})
-dev.copy2pdf(file= "output/stats_plots/photo_chem3d.pdf")
-dev.off()
-#means plot
-# windows()
-# with(photo_chem_campaign, {
-#   s3d <- scatterplot3d(Nmass_notnc.mean, starch.mean, A_mass.mean,   
-#                 type="h", lty.hplot=2, pch=pchs[volume], color=volume,
-#                 main="Photosynthesis vs N and TNC",xlab=nfree, ylab=starchlab, zlab=Amasslab)
-#   s3d.coords <- s3d$xyz.convert(Nmass_notnc.mean, starch.mean,  A_mass.mean)
-#   text(s3d.coords$x, s3d.coords$y,labels=campaign,cex=.75, pos=4) 
-#   legend("right", leglab, inset=.1,     
-#          bty="n", text.font=2,pch=pchs, col=palette(), title=vollab) 
-# })
-
-#full 3d plotting
-photo5 <- subset(photo_chem, volume=="05")
-photo5f <- subset(photo_chem, volume=="05"| volume=="1000")
-photo5f$volume<- droplevels(photo5f$volume)
-rb<- c("red", "blue")
-
-with(photo_chem, plot3d(Nmass_notnc,starch,A_mass, col=volume, size=1, type='s'))
-with(photo5, plot3d(Nmass_notnc,starch,A_mass, col="red", size=1, type='s'))
-with(photo_chem3, plot3d(Nmass_notnc,starch,A_mass, col="blue", size=1, type='s'))
-with(photo5f, plot3d(Nmass_notnc,starch,A_mass, col=rb[volume], size=1, type='s'))
-
-
-library(Rcmdr)
-scatter3d(A_mass~Nmass_notnc+starch, data=photo_chem,fit="smooth", point.col=volume)
-scatter3d(A_mass~Nmass_notnc+starch, data=photo5f,fit="smooth", point.col=rb[volume])
-
-scatter3d(A_mass~Nmass_notnc+starch, data=subset(photo_chem, volume=="1000",fit="smooth"))
-scatter3d(A_mass~Nmass_notnc+starch, data=subset(photo_chem, volume=="05",fit="smooth"))
-scatter3d(A_mass~Nmass_notnc+starch, data=subset(photo_chem, volume=="10",fit="smooth"))
-scatter3d(A_mass~Nmass_notnc+starch, data=subset(photo_chem, volume=="15",fit="smooth"))
-scatter3d(A_mass~Nmass_notnc+starch, data=subset(photo_chem, volume=="20",fit="smooth"))
-scatter3d(A_mass~Nmass_notnc+starch, data=subset(photo_chem, volume=="25",fit="smooth"))
-scatter3d(A_mass~Nmass_notnc+starch, data=subset(photo_chem, volume=="35",fit="smooth"))
-
-with(photo_chem, scatter3d(Nmass_notnc, starch,A_mass, fit="linear"))
 
 #plot predicted and observed in bins------------------------------------------------------
 
@@ -237,75 +165,6 @@ for(i in 1:length(nitrobin3)){
  title(main="Free")
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#complex model mass based--------------------------------------------------------------------
-#if comparing models with different structure then use ML method
-
-#model1 (volume as factor, ID is random effect, and all others are fixed effects)
-Amass_model <- lme(A_mass~ starch*Nmass_notnc*volume, random=~1|ID, data=photo_chem, method="ML")
-#model2 (adds campaign as random effect)
-Amass_model2 <- lme(A_mass~ starch*Nmass_notnc*volume, random=~campaign|ID, data=photo_chem, method="ML")
-
-#model3 (adds campaign as fixed effect)
-Amass_model3 <- lme(A_mass~ campaign*starch*Nmass_notnc*volume, random=~1|ID, data=photo_chem,method="ML")
-
-anova(Amass_model, Amass_model2, Amass_model3)
-
-#USE simply model function to step and delete down to the best model (choosing model with time)
-timemod2 <- simp_mod(Amass_model3)
-timemod3 <- timemod2$model.ml
-timemod4 <- update(timemod3,~. -campaign:starch:Nmass_notnc)
-anova(timemod3, timemod4)
-
-timemod5 <- simp_mod(timemod4)
-timemod6 <- update(timemod5$model.ml,~. -starch:Nmass_notnc)
-Anova(timemod6)
-
-timemod7 <- simp_mod(timemod6)
-finalmod <- timemod7$model.reml
-Anova(finalmod)
-
-#plot the two seperate interactions
-plot(allEffects(finalmod), "campaign:starch:volume", x.var = "starch")
-plot(allEffects(finalmod), "campaign:Nmass_notnc:volume", x.var = "Nmass_notnc")
-xyplot(A_mass ~ starch| volume + factor(campaign), data = photo_chem, type = c("p", "r"))
-xyplot(A_mass ~ Nmass_notnc| volume + factor(campaign), data = photo_chem, type = c("p", "r"))
-
-
-# #orginal model setup
-# Anova(Amass_model)
-# Amm2 <- update(Amass_model, method = "ML")
-# Amm3 <- stepAIC(Amm2)
-# Anova(Amm3)#minimal adequate mod
-# Amm4 <- update(Amm3, method = "REML")
-# Anova(Amm4)
-# #significant effect of Nmass(volume) and Starch (volume) on Amax but no interaction between them
-# summary(Amm4)
-# 
-# plot(allEffects(Amm4), "starch:volume", x.var="starch")
-# plot(allEffects(Amm4), "Nmass_notnc:volume", x.var="Nmass_notnc")
-
-
-
 #---------------------------------------------------------------------------------------
 #plot stuff
 manX <- seq(0, .25, .05)
@@ -387,33 +246,5 @@ d_ply(photo_chem_campaign, .(volume), function(x) c(add_trend_line("starch.mean"
                                                          font=2,cex=.75)))
 dev.copy2pdf(file="output/stats_plots/amaxstarch.pdf")
 dev.off()
-
-
-
-
-#model area based---------------------------------------------------------------------
-
-#variables for area based model
-
-# #Amax per unit leaf area
-# photo_chem$A_area <- with(photo_chem, Photo/area)
-# #tnc on area basis
-# photo_chem$tncarea <- with(photo_chem, ((leafsugar+leafstarch)/area))
-# #n on area bases
-# photo_chem$narealeaf_notnc <- with(photo_chem, nmassleaf_notnc/area) 
-# 
-# #model
-# Aarea_model <- lme(Photo~ tncarea*narealeaf_notnc*volume, random=~1|ID, data=photo_chem)
-# Anova(Aarea_model)
-# Aam2 <- update(Aarea_model, method = "ML")
-# Aam3 <- stepAIC(Aam2)
-# Anova(Aam3)
-# Aam4 <- update(Aam3, method = "REML")
-# Anova(Aam4)
-
-# summary(Aam4)
-# plot(allEffects(Aam4), x.var="narealeaf_notnc")
-# summary(photo_chem)
-# plot(Photo ~ narealeaf_notnc, data = photo_chem, col = volume, pch = 16)
 
 
