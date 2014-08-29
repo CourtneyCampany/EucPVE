a9c6243d92d82a5604b66edc4ef68e9180dfeccf
Courtney Campany <courtneycampany@gmail.com>
2014-08-29 17:15:57 +1000

    added sla and trt (still need to use)
    added respiration components
    
    added LMF and leaf mass to model calc

--- cgain_model.R
+++ cgain_model.R
@@ -26,6 +26,8 @@ mean_leafnum <- mean(seedling_pre$leaf_numb)
 #mean lma and leafarea (apply to intial leaf )-----------------------------------------
 #need to pull leaf area from somewhere
 lma <- read.csv("calculated data/leafmassarea.csv")
+sla_vol <- summaryBy(sla ~volume, data=lma, FUN=mean, keep.names=TRUE)
+
 #lma_mean <- mean(lma$massarea)
 leafarea_mean <- (mean(lma$area))/10000
 #average starting leaf area
@@ -54,6 +56,7 @@ lma_mean <- mean(lma$massarea)#average lma from harvest
 LA_start <- (mean_leafnum * leafarea_mean) #(m2)
 mass_mean <- mean(seedling_pre$seedling_mass)
 Cday <- as.vector(Aleaf_agg[,2]) #vector of 7 treaments in order
+sla_trt <- as.vector(sla_vol[,2])
 #sla <-  1/lma_mean
 #volumeid <- as.factor(c(5,10,15,20,25,35,"free"))
 
@@ -62,7 +65,11 @@ productionmodel <- function(leafrac = .18,
                     sla = .0102,
                     gCday = .2,
                     conversionEfficiency = 0.65,
+                    fr_resp = .010368, #gC/gFroot day Marsden et al
+                    cr_resp = .00124, #gC/gCroot day
+                    wd_resp = .00269, #gc/gwood day Ryan et al. eucs 1yr
                     numdays=120,
+                    lma = 97.5,
                     returnwhat=c("lastval","all")
                     ){
   
@@ -74,26 +81,38 @@ productionmodel <- function(leafrac = .18,
   biomass <- vector()
   biomass[1] <- mass_mean
   
+  leafmass <- vector()
+  leafmass[1] <- LA_start*lma_mean
+  
+  LMF <- vector()
+  LMF[1] <- (LA_start*lma_mean)/mass_mean
+  
   if(length(gCday) == 1)gCday <- rep(gCday,numdays)
   if(length(gCday) < numdays)stop("Need at least ",numdays," photosynthesis values.")
   
   #run model simulation------------------------------------------------------------------
   for (i in 2:numdays) {
     biomassprod <- leafarea[i-1] * gCday[i]/conversionEfficiency  # gc day-1
-    biomass[i] <- biomass[i-1] + biomassprod
+    biomass[i] <- biomass[i-1] + biomassprod - ((biomass[i-1]*fr_resp)+(biomass[i-1]*cr_resp)+(biomass[i-1]*wd_resp))
     leafarea[i] <- leafarea[i-1] + (biomassprod*leafrac*sla)
+    leafmass[i] <- leafarea[i] * lma
+    LMF[i] <- leafmass[i]/biomass[i]
   }
 
   if(returnwhat == "lastval")
-    return(c(biomass=biomass[numdays],leafarea=leafarea[numdays]))
+    return(c(biomass=biomass[numdays],leafarea=leafarea[numdays], leafmass = leafmass[numdays], LMF = LMF[numdays]))
   
   if(returnwhat == "all")
-    return(list(biomass=biomass,leafarea=leafarea))
+    return(list(biomass=biomass,leafarea=leafarea, leafmass = leafmass, LMF = LMF))
   
 }
 
 #run mean gCday for each volume through model
-modelmass <- as.data.frame(do.call(rbind, mapply(productionmodel, gCday=Cday,leafrac=.25, SIMPLIFY=FALSE)))
+modelmass <- as.data.frame(do.call(rbind, mapply(productionmodel, gCday=Cday,
+                                                 sla=sla_trt,leafrac=.25, SIMPLIFY=FALSE)))
+
+modelmass_all <- as.data.frame(do.call(rbind, mapply(productionmodel, gCday=Cday,
+                                                 sla=sla_trt,leafrac=.25, returnwhat="all",SIMPLIFY=FALSE)))
 
 # plot(biomass~Date, data=Cgain_25)
 # plot(leafarea~Date, data=Cgain_25)
@@ -101,6 +120,8 @@ modelmass <- as.data.frame(do.call(rbind, mapply(productionmodel, gCday=Cday,lea
 
 plot(Cday, modelmass$biomass, ylim=c(0,200))
 points(Cday, mass_actual$mass, col="red")
+
+#add A and plot mass, leafmass, and LMF vs A
 
 
 
