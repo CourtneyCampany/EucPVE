11dfefea8b147880ceae8a96f97bae5a8cb90310
Courtney Campany <courtneycampany@gmail.com>
2014-09-03 10:46:23 +1000

    fixed source to read.csv and cleaned with SE function

--- Stats/harvest stats.R
+++ Stats/harvest stats.R
@@ -1,26 +1,19 @@
 source("functions and packages/load packages.R")
+source("functions and packages/functions.R")
 
 #read data
-source("calculated data/seedling mass.csv")
-
+seedlingmass <- read.csv("calculated data/seedling mass.csv")
 
 #treatment means for total mass and ratios
 finalmass <- subset(seedlingmass, select = c("ID", "volume", "totalmass", 
                                              "rootshoot", "frootleaf"))
-mass_agg <- summaryBy( .~ volume , data = finalmass,  FUN=c(mean,sd,length))
-
-mass_agg$massSE <- with(mass_agg, totalmass.sd/sqrt(totalmass.length))
-mass_agg$rootshootSE <- with(mass_agg, rootshoot.sd/sqrt(rootshoot.length))
-mass_agg$frootleafSE <- with(mass_agg, frootleaf.sd/sqrt(frootleaf.length))
-mass_agg$finerootSE <- with(mass_agg, frootleaf.sd/sqrt(frootleaf.length))
-mass_agg$shootSE <- with(mass_agg, frootleaf.sd/sqrt(frootleaf.length))
+mass_agg <- summaryBy( .~ volume , data = finalmass,  FUN=c(mean,se))
 
 #Stats
 totalmasslm <- lm(totalmass ~ as.factor(volume), data=finalmass)
 rootshootlm <- lm(rootshoot ~ as.factor(volume), data=finalmass)
 frootleaflm <- lm(frootleaf ~ as.factor(volume), data=finalmass)
 
-getP <- function(x)anova(x)[[5]][1]
 getP(totalmasslm)
 getP(rootshootlm)
 getP(frootleaflm)
@@ -35,8 +28,4 @@ getP(totalmass_potsize)
 getP(rootshoot_potsize)
 getP(frootleaf_potsize)
 
-#Plotting
 
-with(finalmass, bargraph.CI(as.factor(volume), totalmass))
-with(finalmass, bargraph.CI(as.factor(volume), rootshoot))
-with(finalmass, bargraph.CI(as.factor(volume), frootleaf))
