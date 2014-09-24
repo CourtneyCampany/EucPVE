36e439cd61bdb7d700a3a4a88ccac885e75ad47d
Courtney Campany <courtneycampany@gmail.com>
2014-09-24 18:10:05 +1000

    added small graph for insert into photochem figure

--- Stats/leafN_stats.R
+++ Stats/leafN_stats.R
@@ -90,7 +90,7 @@ dev.off()
 windows()
 plot(Narea.mean ~ Date, data = Nleaf_means, col=volume, cex=2, pch=pchs[volume], 
      ylim=c(0,1.3), ylab="", xlab="")
-title(ylab=narealab, mgp=ypos)
+title(ylab=narealab, mgp=ypos2)
 title(main=datemean, line=-1.5, font.main=1, adj=.05, cex.main=1)
 d_ply(Nleaf_means, .(volume), function(x) add_trend_line("Date", "Narea.mean", x, ))
 legend("bottomleft", leglab, pch=pchs,text.font=1, inset=0.02, title=vollab, col=palette(), bty='n')
@@ -99,6 +99,19 @@ dev.copy2pdf(file="output/stats_plots/Narea_means.pdf")
 dev.off()
 
 
+N_end <- subset(leafchem_noNA, Date == "2013-05-16")
+
+#windows()
+png(filename = "output/presentations/LeafN_area.png", width = 3.25, height = 2.25, units = "in", res= 400, bg = "transparent")
+par(cex.axis=.5, cex.lab=.5, tck=.02)
+bar(Nmass, c(volume), leafchem_noNA, col=palette(), half.errbar=FALSE, xlab="", 
+    legend=FALSE, ylim=c(0, 0.008), ylab="", mgp = c(3, .1, 0))
+#legend("topleft", leglab, pch=15,text.font=1, inset=0.02, title=vollab, col=palette(), bty='n')
+title(ylab=nitro, mgp=ypos2)
+
+dev.off()
+
+ypos2 <- c(.75,1,0)
 
 #----------------------------------------------------------------------------------
 #fit seperate linear regression models for each volume through time for each pot
