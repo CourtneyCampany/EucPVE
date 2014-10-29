e11af2689fffbd239ca6f94dadb623d1598c0044
Courtney Campany <courtneycampany@gmail.com>
2014-10-29 17:40:59 +1100

    added plots of lmf model and allocation barplot

--- master_scripts/poorter_mass.R
+++ master_scripts/poorter_mass.R
@@ -2,9 +2,8 @@ source("functions and packages/startscripts.R")
 
 library(visreg)
 library(multcomp)
-library(quantmod)
-library(reshape)
 
+#harvest mass dfr
 seedlingmass<- read.csv("calculated data/seedling mass.csv") 
 
 # rmf calculate
@@ -18,23 +17,30 @@ mass_agg_nofree <- subset(mass_agg, volume != 1000)
 #volume as factor after new variable calculations
 seedlingmass$volume <- as.factor(seedlingmass$volume)
 
-
 seedlingmass$Msr <- with(seedlingmass, stemmass + Croot + fineroot)
+seedlingmass$logMsr <- log10(seedlingmass$Msr)
+seedlingmass$logLeaf <- log10(seedlingmass$leafmass)
 
-
+###Test whether leaf mass fraction is different with treatment
+  #must account for variation in plant size
 library(smatr)
-sm1 <- sma(leafmass ~ Msr + volume, log="xy", data=seedlingmass)
-summary(sm1)
+
+Mf_mod <- sma(leafmass ~ Msr + volume, log="xy", data=seedlingmass)
+summary(Mf_mod)
 # here the slope is 1.13, which is the exponent b1 in Mf = b0*Msr^b1
 # if b1 > 1, then we have an increasing LMF with plant size.
 
-sm2 <- sma(leafmass ~ Msr * volume, log="xy", data=seedlingmass)
-summary(sm2)
+Mf_mod2 <- sma(leafmass ~ Msr * volume, log="xy", data=seedlingmass)
+summary(Mf_mod2)
 
-# probably include this
-plot(sm2)
+#PLotting of LMF model (use as a 2panel with allocation stacked)
+plot(Mf_mod2, xlab= expression(log[10]~Stem+Root~Mass~~(g)), ylab="", col=as.factor(seedlingmass$volume), 
+     pch=pchs[as.factor(seedlingmass$volume)])
+  title(ylab=expression(log[10]~Leaf~Mass~~(g)), mgp=ypos)
+  legend("topleft", leglab, pch=pchs,text.font=3, inset=0.02, title=expression(Pot~volume~(l)), 
+       col=palette(), bty='n')
 # this shows that LMF is higher for free seedlings because they were bigger.
-
+with(seedlingmass, plot(logLeaf~logMsr, col=volume))
 
 
 
@@ -52,23 +58,27 @@ mass_perc <- data.frame(volume=as.factor(mass_agg$volume),
             fr_perc = with(mass_agg, fineroot/totalmass))
 
 
-x <- mass_perc[,2:5]
-i <- c(4,3,1,2)
-barplot(t(as.matrix(x))[i,], names.arg=mass_perc$volume)
+mass_perc2 <- mass_perc[,2:5]
+i <- c(4,3,1,2) #correct order of variables
+treecols <- c("darkgoldenrod4","lightgoldenrod3",  "olivedrab","forestgreen")
+treecols2 <- c("forestgreen","olivedrab", "lightgoldenrod3","darkgoldenrod4")
+treelab <- c("Leaves", "Stems", "Coarse Roots", "Fine Roots")
+
+
+#plot
+par(mar = c(5.1, 4.1, 4.1, 7.3), xpd = TRUE)
+barplot(t(as.matrix(mass_perc2))[i,], names.arg=leglab, col=treecols, width=2, xlab= "")
+  legend("topright", inset = c(-0.225, 0), fill = treecols2, legend=treelab)
 
 
 
 
-mass_melt <- melt(mass_perc, id.vars = "volume", measure.vars= c("leaf_perc","stem_perc", 
-                    "fr_perc", "cr_perc"), variable_name="component")
 
-mass_wide <- reshape(mass_melt,timevar="volume",  v.names="value", idvar = "component",
-                             direction = "wide")
+par(mar = c(5.1, 4.1, 4.1, 7.1), xpd = TRUE)
+barplot(prop, col = heat.colors(length(rownames(prop))), width = 2)
+legend("topright", inset = c(-0.25, 0), fill = heat.colors(length(rownames(prop))), 
+       legend = rownames(data))
 
-barplot(t(mass_wide))
-
-barplot(matrix(mass_wide), beside=FALSE)
-      
 
 ### Analyze Mass increase with each fold increase from 5l to test Poorter------------------------
 
