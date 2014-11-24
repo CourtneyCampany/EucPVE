bf453de1928d87acd137e09d4b66964c76cfa0ae
Courtney Campany <courtneycampany@gmail.com>
2014-11-24 11:49:56 +1100

    added leaf length from harvest and tested the relationship with leaf area
    pretty bad so will stick with mean leaf area and leaf count

--- clean Scripts/harvest leaves.R
+++ clean Scripts/harvest leaves.R
@@ -5,29 +5,61 @@ source("read data scripts/harvest read data.R")
 source("read data scripts/survey read data.R")
 
 leafharvest <- subset(leafharvest, !is.na(leaf_area))
-leafharvest$areaperleaf <- with(leafharvest, leaf_area/leaf_count)
-leafharvest$totalarea <- with(leafharvest, leaf_area + newleaf_area)
-leafharvest$totalcount <- with(leafharvest, leaf_count + newleaf_count)
-
+  leafharvest$areaperleaf <- with(leafharvest, leaf_area/leaf_count)
+  leafharvest$totalarea <- with(leafharvest, leaf_area + newleaf_area)
+  leafharvest$totalcount <- with(leafharvest, leaf_count + newleaf_count)
 #merge with plot summary
-leafharvest <- merge(leafharvest, plotsumm, by = c("pot", "plot", "ID"))
-leafharvest$volume <- as.factor(leafharvest$volume)
+leafharvest <- merge(leafharvest, plotsumm[,3:4], by ="ID")
+  leafharvest$volume <- as.factor(leafharvest$volume)
+
+#harvest leaf length
+ll_harvest <- read.csv("raw data/harvest leaf length.csv")
+  ll_harvest <- melt(ll_harvest, id=c("pot", "plot"))
+  ll_harvest$ID <- with(ll_harvest, paste(plot, pot, sep="-"))
+  names(ll_harvest)[4]<- "leaf_length"
+ll_agg <- aggregate(leaf_length ~ ID, ll_harvest, FUN=mean,  na.action = na.omit)
+
+####merge leafharvest with leaf length mean
+leafharvest <- merge(leafharvest, ll_agg, by ="ID")
+  row.names(leafharvest) <- NULL
 
 write.csv(leafharvest[,c(3, 13:15)], "calculated data/LA_harvest.csv", row.names=FALSE)
+
 #treatment means for total area and count
 leafharvest_agg <- aggregate(cbind(totalarea, totalcount) ~ volume , data = leafharvest, FUN = mean)
 
-#calculate leaf area trough time
+
+####relationship between leaf length and area-------------------------------------------------------------------
+area_length <- leafharvest[,c(1, 12, 15:16)]
+  area_length$ratio <- with(area_length, areaperleaf/leaf_length)
+
+ll_mod <- lm(areaperleaf~leaf_length, data=area_length)
+  extract_func(ll_mod)
+ll_mod_1000<- lm(areaperleaf~leaf_length, data=area_length, subset=volume==1000)
+extract_func(ll_mod_1000)
+
+ll_mod2 <- lm(ratio~volume, data=area_length)
+  anova(ll_mod2)
+  summary(ll_mod2)
+  library(visreg)
+  visreg(ll_mod2)
+
+plot(areaperleaf~leaf_length, ylim=c(0,40), xlim=c(0,15),col=volume,pch=16,data=area_length, subset=volume==1000)
+abline(ll_mod)
+####half the volumes have a pretty bad relationship, so continue using mean leafarea and leaf count 
+
+
+#calculate leaf area trough time------------------------------------------------------------------------------
 leafindex <- subset(leafharvest, select = c("ID", "areaperleaf", "volume"))
 
 #format leaf count data
 leafcount$X1.21.2013   <- as.integer(leafcount$X1.21.2013)
-leafcount <- melt(leafcount, id=c("plot", "pot"))
-names(leafcount)[3:4] <- c("Date", "count")
-leafcount$Date <- gsub ("X", "", leafcount$Date)
-leafcount$Date <- as.Date(leafcount$Date, format = "%m.%d.%Y")
-leafcount <- subset(leafcount, !is.na(count))
-leafcount$ID <- paste(leafcount$plot, leafcount$pot, sep = "-")
+  leafcount <- melt(leafcount, id=c("plot", "pot"))
+  names(leafcount)[3:4] <- c("Date", "count")
+  leafcount$Date <- gsub ("X", "", leafcount$Date)
+  leafcount$Date <- as.Date(leafcount$Date, format = "%m.%d.%Y")
+  leafcount <- subset(leafcount, !is.na(count))
+  leafcount$ID <- paste(leafcount$plot, leafcount$pot, sep = "-")
 
 leaftime <- merge(leafindex, leafcount, by = "ID")
 
