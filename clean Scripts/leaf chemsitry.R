5e111eac97d607af81c484bb16d60e1297493b0c
Courtney Campany <courtneycampany@gmail.com>
2014-11-26 09:36:52 +1100

    added stats for leaf N or %N on mean and on first campaign date

--- clean Scripts/leaf chemsitry.R
+++ clean Scripts/leaf chemsitry.R
@@ -5,8 +5,6 @@ source("functions and packages/startscripts.R")
 source("read data scripts/harvest read data.R")
 leafCN <- read.csv("raw data/leafCN.csv")
 leafarea <- read.csv("calculated data/leafareabypot.csv")
-TNC <- read.csv("raw data/TNC.csv")
-
 
 lma <- read.csv("raw data/seedling leaf mass area.csv")
 lma$ID <- paste(lma$plot, lma$pot, sep = "-")
@@ -19,35 +17,47 @@ CN_leaf <- merge(CN_leaf, plotsumm[3:4], all=TRUE)
 
 lma <- add_campaign_date(lma)
 
-#TNC is means by volume now, maybe go back to raw data
-TNC <- add_campaign_noID(TNC)
-
 
 #calculate C and N content of leaves
 leaf_chem <- merge(CN_leaf, lma, all=TRUE)
+  leaf_chem$leafC <- with(leaf_chem, mass*Cperc)
+  leaf_chem$leafN <- with(leaf_chem, mass*Nperc)
+  leaf_chem$volume <- as.factor(leaf_chem$volume)
 
-leaf_chem$leafC <- with(leaf_chem, mass*Cperc)
-leaf_chem$leafN <- with(leaf_chem, mass*Nperc)
 
-plot(leafC~Date, col=volume, data=leaf_chem)
+###stats on leaf N---------------------------------------------------------------------------
+leaf_chem2 <- leaf_chem[complete.cases(leaf_chem),]
+  require(car)
+  leaf_chem2$Ntrans <- with(leaf_chem2, asin(sqrt(Nperc/100)))
+  leaf_chem2$Ntrans2 <- logit(leaf_chem2$Nperc, percents=TRUE )
+  leaf_chem2$volume <- as.factor(leaf_chem2$volume)
+
+require(nlme)
+require(visreg)
+library(multcomp)
+
+#Nperc
+nperc_lm <- lme(leafN ~ volume, random= ~1|ID, data=leaf_chem2)
+anova(nperc_lm)
+summary(nperc_lm)
+
+tukey_N<- glht(nperc_lm, linfct = mcp(volume = "Tukey"))
+cld(tukey_N)
+visreg(nperc_lm)
+
+#N_perc first date
+
+nperc_lm2 <- lme(leafN ~ volume, random= ~1|ID, data=leaf_chem2, subset=Date=="2013-03-07")
+anova(nperc_lm2)
+summary(nperc_lm2)
+visreg(nperc_lm2)
+tukey_N2<- glht(nperc_lm2, linfct = mcp(volume = "Tukey"))
+cld(tukey_N2)
+
+####plotting---------------------------------------------------------------------------------
+N_agg <- summaryBy(Nperc+leafN ~volume + Date, data=leaf_chem, FUN=mean, keep.names=TRUE)
+
+plot(Nperc~Date, pch=pchs[volume],col=volume, data=N_agg)
 plot(leafN~Date, pch=pchs[volume],col=volume, data=leaf_chem)
 #analyze leaf chem with Asat, area, mass on campaign dates
 
-
-leaf_chem <- merge(leaf_chem, TNC)
-
-
-
-
-
-#requires Dates of A campaigns and leaf mass predictons
-
-
-
-#then summarise though time with starchs and sugars
-
-
-leaf_harvest <- subset(harvest_mass, select = C("ID", "leafmass", "leaf_area", "leaf_count"))
-
-
-#TNC (need to convert the amount of tnc (mg/g dwt) to canopy content to compare with C and N)
