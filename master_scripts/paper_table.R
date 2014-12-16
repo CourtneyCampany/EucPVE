0efcca2c1049ced13b9de85fb5e509230ad3f608
Courtney Campany <courtneycampany@gmail.com>
2014-12-16 16:18:05 +1100

    table know is only seedling data

--- master_scripts/paper_table.R
+++ master_scripts/paper_table.R
@@ -1,85 +1,61 @@
-#this script creates the data table for the mauscripts
+#this script creates the plant based data table for the mauscripts
 library(doBy)
 source("functions and packages/functions.R")
 source("functions and packages/plot objects.R")
 
-#start with harvest dfr
+###variables:   Mass, SLA, SRL, Starct, Sugar, leaf N
+
+#1: seedling mass
 harvestmass <- read.csv("calculated data/seedling mass.csv") 
 
-#####start with means of totalmass
+  #####start with means of totalmass
 table1 <- summaryBy(totalmass~volume, data=harvestmass, FUN=c(mean, se))
   names(table1)[2:3]<- c("Seedling mass", "Seedling mass_se")
 
-# ##gas exchange parameters jmax, vcmax, g1,Rd 
-# phys <- read.csv("calculated data/jmax_vcmax.csv")
-# phys_agg <- summaryBy(Jmax.mean+Vcmax.mean ~ volume, data=phys, FUN=c(mean, se))
-#   names(phys_agg)[2:5]<- c("Jmax", "Vcmax", "Jmax_se", "Vcmax_se")
-
-####these have no se, only 7 values move to 
-# g1 <- read.csv("calculated data/g1_pred.csv")
-# g1_agg <- summaryBy(g1_vol ~ volume, data=g1, FUN=c(mean, se))
-#   names(g1_agg)[2:3]<- c("G1", "G1_se")
-
-#Amax
-A_means <- read.csv("calculated data/A_treatment_means.csv")
-  A_means$volume <- as.numeric(c("5", "10", "15", "20", "25", "35", "1000"))
-Amax <- A_means[, c(1, 4:5)]
-
-####add Amax to table
-table1 <- merge(table1, Amax)
-
-####add SRL and add to table
+#2:SRL
 srl <- read.csv("calculated data/srl_means.csv")
 
 table1 <- merge(table1, srl)
 
 
-####add SLA, starch, sugars, Nmass
+#3-6: SLA, starch, sugars, Nmass
 
 photo_chem <- read.csv("calculated data/Amax_chem.csv")
-#run volume format func
-photo_chem<- vollab_func(photo_chem)
+  #run volume format func
+  photo_chem<- vollab_func(photo_chem)
 
-leaf_param <- photo_chem[,c(1:3,9,11:12, 14:17 )]
-leaf_param$volume <- gsub("05", "5", leaf_param$volume)
+  leaf_param <- photo_chem[,c(1:3,9,11:12, 14:17 )]
+  leaf_param$volume <- gsub("05", "5", leaf_param$volume)
 
-leaf_param_agg <- summaryBy(sla +starch+sugars+ Nmass~ volume, data=leaf_param, FUN=c(mean, se))
+  leaf_param_agg <- summaryBy(sla +starch+sugars+ Nmass~ volume, data=leaf_param, FUN=c(mean, se))
 
 table1 <- merge(table1,leaf_param_agg)
 
-####add Rd
-rd <- read.csv("calculated data/Rd_leaf.csv")
-rd_agg <- summaryBy(resppermass ~ volume, data=rd, FUN=c(mean, se))
 
-table1 <- merge(table1, rd_agg)
-
-
-###now sort the table into correct var + se, 
-pve_data <-table1[,c(1,2,3,4,5,6,7,8,12,9,13,10,14,11,15,16,17)]
+###now sort the table into correct var + se--------------------------------------------------------------------
+tree_tab <-table1[,c(1,2,3,4,5,6,10,7,11,8,12,9,13)]
 
 #seperate dfr in two with mean and se, omit volume for now
-pve_means <- pve_data[, c(2,4,6,8,10,12,14,16)]
-pve_se <- pve_data[, c(3,5,7,9,11,13,15,17)]
+tree_means <- tree_tab[, c(2,4,6,8,10,12)]
+tree_se <- tree_tab[, c(3,5,7,9,11,13)]
 
 ###now paste together and round
-dat1 <- data.frame(paste0(signif(pve_means[,1], 3), " (", signif(pve_se[,1],2),")"))
-dat2 <- data.frame(paste0(signif(pve_means[,2], 3), " (", signif(pve_se[,2],2),")"))
-dat3 <- data.frame(paste0(signif(pve_means[,3], 2), " (", signif(pve_se[,3],2),")"))
-dat4 <- data.frame(paste0(signif(pve_means[,4], 2), " (", signif(pve_se[,4],2),")"))
-dat5 <- data.frame(paste0(signif(pve_means[,5], 2), " (", signif(pve_se[,5],1),")"))
-dat6 <- data.frame(paste0(signif(pve_means[,6], 2), " (", signif(pve_se[,6],1),")"))
-dat7 <- data.frame(paste0(signif(pve_means[,7], 2), " (", signif(pve_se[,7],2),")"))
-dat8 <- data.frame(paste0(signif(pve_means[,8], 2), " (", signif(pve_se[,8],1),")"))
+dat1 <- data.frame(paste0(signif(tree_means[,1], 3), " (", signif(tree_se[,1],2),")"))
+dat2 <- data.frame(paste0(signif(tree_means[,2], 2), " (", signif(tree_se[,2],2),")"))
+dat3 <- data.frame(paste0(signif(tree_means[,3], 2), " (", signif(tree_se[,3],2),")"))
+dat4 <- data.frame(paste0(signif(tree_means[,4], 2), " (", signif(tree_se[,4],2),")"))
+dat5 <- data.frame(paste0(signif(tree_means[,5], 2), " (", signif(tree_se[,5],1),")"))
+dat6 <- data.frame(paste0(signif(tree_means[,6], 2), " (", signif(tree_se[,6],2),")"))
 
-pve_table <- cbind(leglab, dat1)
-  pve_table <- cbind(pve_table, dat2)
-  pve_table <- cbind(pve_table, dat3)
-  pve_table <- cbind(pve_table, dat4)
-  pve_table <- cbind(pve_table, dat5)
-  pve_table <- cbind(pve_table, dat6)
-  pve_table <- cbind(pve_table, dat7)
-  pve_table <- cbind(pve_table, dat8)
 
-write.csv(pve_table, "master_scripts/pve_table1.csv", row.names=FALSE)
+pve_table1 <- cbind(leglab, dat1)
+  pve_table1 <- cbind(pve_table1, dat2)
+  pve_table1 <- cbind(pve_table1, dat3)
+  pve_table1 <- cbind(pve_table1, dat4)
+  pve_table1 <- cbind(pve_table1, dat5)
+  pve_table1 <- cbind(pve_table1, dat6)
+
+
+write.csv(pve_table1, "master_scripts/pve_table1.csv", row.names=FALSE)
 
 
