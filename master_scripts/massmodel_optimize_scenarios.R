3e21e89ba5dfb6c39325d7d70d1b28feb14e68ad
Courtney Campany <courtneycampany@gmail.com>
2015-05-12 18:40:01 +1000

    added scenario plots to mass model.. only need to figure out how to show old model on plot

--- master_scripts/massmodel_optimize_scenarios.R
+++ master_scripts/massmodel_optimize_scenarios.R
@@ -1,6 +1,8 @@
 library(doBy)
 library(scales)
 
+source("functions and packages/plot objects.R")
+
 source("gC_day_model/model_start.R")
 source("functions and packages/massmodel_LAconstrain.R")
 source("functions and packages/massmodel.R")
@@ -52,10 +54,10 @@ O.lmf <- function(leaffrac, ..., returnhow=c("objective","output")){
   
 }
 
-#6. Visualize if there is an optimum
-lfs <- seq(0.05,0.3, length=101)
-test_opt <- mapply(O.lmf, leaffrac=lfs)
-plot(lfs,test_opt)  
+# #6. Visualize if there is an optimum
+# lfs <- seq(0.05,0.3, length=101)
+# test_opt <- mapply(O.lmf, leaffrac=lfs)
+# plot(lfs,test_opt)  
 
 #7. Rerun model to see correspondence 
 
@@ -98,7 +100,7 @@ for(i in 1:7){
                                                   crfrac=(1-leaffrac_trt[i])/3, 
                                                   stemfrac=(1-leaffrac_trt[i])/3,
                                                   M_slope=Mcoef$b[i], M_intercept= Mcoef$intercept[i],
-                                                  returnwhat="all"))
+                                                  returnwhat="lastval"))
 }   
   
   
@@ -127,30 +129,81 @@ for(i in 1:7){
   wd_resp_down <- c(rep(wd_down,6),wd_default)
   
   #root respiration up
-  rootresp_up<- list()
+  resp_up<- list()
   for(i in 1:7){
-  rootresp_up[[i]] <- data.frame(productionmodel2(gCday=c120_trt[[i]][[1]], lma=lma_trt[i],frfrac=opt_ofrac, 
+  resp_up[[i]] <- data.frame(productionmodel2(gCday=c120_trt[[i]][[1]], lma=lma_trt[i],frfrac=opt_ofrac, 
                                                       crfrac=opt_ofrac, stemfrac=opt_ofrac,leaffrac=opt_lmf,
                                                       M_slope=Mcoef$b[i], M_intercept= Mcoef$intercept[i],
                                                       fr_resp=fr_resp_up[i], cr_resp= cr_resp_up[i],
                                                       wd_resp=wd_resp_up[i],
-                                                      returnwhat="all"))
+                                                      returnwhat="lastval"))
     }   
   
   #root respiration down
-  rootresp_down<- list()
+  resp_down<- list()
   for(i in 1:7){
-  rootresp_down[[i]] <- data.frame(productionmodel2(gCday=c120_trt[[i]][[1]], lma=lma_trt[i],frfrac=opt_ofrac, 
+  resp_down[[i]] <- data.frame(productionmodel2(gCday=c120_trt[[i]][[1]], lma=lma_trt[i],frfrac=opt_ofrac, 
                                                         crfrac=opt_ofrac, stemfrac=opt_ofrac,leaffrac=opt_lmf,
                                                         M_slope=Mcoef$b[i], M_intercept= Mcoef$intercept[i],
                                                         fr_resp=fr_resp_down[i], cr_resp= cr_resp_down[i],
                                                         wd_resp=wd_resp_down[i],
-                                                        returnwhat="all"))
+                                                        returnwhat="lastval"))
   }  
   
   
   
 ####PLOT scenarios
   
+  ####add actual mass and Cday scaled
+  ##mean daily carbon gain scales
+  C_stnd <- read.csv("calculated data/model_runs/gCday_means.csv")
+    C_stnd$C_stnd_free <- with(C_stnd, carbon_day/carbon_day[7])
+    
+    ##modelled biomass scaled
+    C_stnd$modelmass <- c(optmassmodel[[1]][1,1], optmassmodel[[2]][1,1], optmassmodel[[3]][1,1], optmassmodel[[4]][1,1], 
+                          optmassmodel[[4]][1,1],optmassmodel[[6]][1,1],optmassmodel[[7]][1,1])
+    C_stnd$model_stnd_free <- with(C_stnd, modelmass/modelmass[7])
   
+    ##modelled biomass scaled (scenario 1)
+    C_stnd$harvest_lmf <- c(harvest_lmf[[1]][1,1], harvest_lmf[[2]][1,1], harvest_lmf[[3]][1,1], harvest_lmf[[4]][1,1], 
+                          harvest_lmf[[4]][1,1],harvest_lmf[[6]][1,1],harvest_lmf[[7]][1,1])
+    C_stnd$lmf_stnd_free <- with(C_stnd, harvest_lmf/harvest_lmf[7])
+    ##modelled biomass scaled (scenario 1)
+    C_stnd$resp_up <- c(resp_up[[1]][1,1], resp_up[[2]][1,1], resp_up[[3]][1,1], resp_up[[4]][1,1], 
+                        resp_up[[4]][1,1],resp_up[[6]][1,1],resp_up[[7]][1,1])
+    C_stnd$resp_down <- c(resp_down[[1]][1,1], resp_down[[2]][1,1], resp_down[[3]][1,1], resp_down[[4]][1,1], 
+                          resp_down[[4]][1,1],resp_down[[6]][1,1],resp_down[[7]][1,1])
+    
+    C_stnd$resp_up_stnd_free <- with(C_stnd, resp_up/resp_up[7])
+    C_stnd$resp_down_stnd_free <- with(C_stnd, resp_down/resp_down[7])
+    
+    
+  
+  #harvest mass (treatment means)
+  mass_actual <- read.csv("calculated data/harvest_mass_means.csv")
+  mass_actual$Date <- as.Date("2013-05-21")
+  mass_actual$mass_adj <- with(mass_actual, mass/mass[7])
+  
+  
+  #scenario 1
+  plot(C_stnd$lmf_stnd_free ~ C_stnd$C_stnd_free , xlim=c(1,.6), ylim=c(0, 1),pch=pch2,col=palette(),cex=1.6,
+       xlab=expression(Mean~Daily~Carbon~Assimilation~Scaled[free]),
+       ylab= expression(Plant~Carbon~Scaled[free]))
+  points(mass_actual$mass_adj ~ C_stnd$C_stnd_free , pch=pchs,col=palette(),cex=1.6)
+  points(C_stnd$model_stnd_free ~ C_stnd$C_stnd_free, pch=pch2, col=palette(),cex=.8)
+#   text(.6,.01,"(b)", cex=1.2)
+#   legend("topright", simleg, pch=simpch,text.font=1,   inset=0.025,bty='n',cex=1.0)
+  
+  #scenario 2 
+  plot(C_stnd$resp_up_stnd_free ~ C_stnd$C_stnd_free , xlim=c(1,.6), ylim=c(0, 1),pch=pch2,col=palette(),cex=1.6,
+       xlab=expression(Mean~Daily~Carbon~Assimilation~Scaled[free]),
+       ylab= expression(Plant~Carbon~Scaled[free]))
+  points(mass_actual$mass_adj ~ C_stnd$C_stnd_free , pch=pchs,col=palette(),cex=1.6)
+  points(C_stnd$model_stnd_free ~ C_stnd$C_stnd_free, pch=pch2, col=palette(),cex=.8)
+  
+  plot(C_stnd$resp_down_stnd_free ~ C_stnd$C_stnd_free , xlim=c(1,.6), ylim=c(0, 1),pch=pch2,col=palette(),cex=1.6,
+       xlab=expression(Mean~Daily~Carbon~Assimilation~Scaled[free]),
+       ylab= expression(Plant~Carbon~Scaled[free]))
+  points(mass_actual$mass_adj ~ C_stnd$C_stnd_free , pch=pchs,col=palette(),cex=1.6)
+  points(C_stnd$model_stnd_free ~ C_stnd$C_stnd_free, pch=pch2, col=palette(),cex=.8)
   
