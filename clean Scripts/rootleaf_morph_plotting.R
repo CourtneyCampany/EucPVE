8fe9ad722ae799f4526346ecb6c95397fdd44b35
Courtney Campany <courtneycampany@gmail.com>
2015-07-21 10:56:01 +1000

    messed with srl stats and output
    added srl changes in manuscript

--- clean Scripts/rootleaf_morph_plotting.R
+++ clean Scripts/rootleaf_morph_plotting.R
@@ -4,39 +4,24 @@ source("functions and packages/startscripts.R")
 plotsumm <- read.csv("raw data/plot_summary.csv")
   plotsumm$ID <- paste(plotsumm$plot, plotsumm$pot, sep = "-")
 
-##SLA[tncfree]------------------------------------------------------------------------------------------
-
-sla_tnc <- read.csv("calculated data/TNC_content")
-  sla_tnc$volume <- as.factor(sla_tnc$volume) 
-  sla_tnc$Date <- as.Date(sla_tnc$Date)
-  sla_tnc$sla <- with(sla_tnc, area/mass)
-  sla_tnc$lma <- with(sla_tnc, mass/area)
-
-leaf_TNCfree <- subset(sla_tnc, select = c("ID", "volume", "sla_free", "lma_free","sla", "lma","Date"))
-  volorder<-order(leaf_TNCfree$volume, by=leaf_TNCfree$Date)
-  leaf_TNCfree <- leaf_TNCfree[volorder,]
-
-#treatment means
-sla_tnc_campaign <- summaryBy(sla_free+lma_free+ sla+ lma ~ volume+Date, data=leaf_TNCfree, FUN=c(mean, se))
-#treatment means
-sla_tnc_agg <- summaryBy(sla_free+lma_free+ sla+ lma~volume, data=leaf_TNCfree, FUN=c(mean, se))
-
 #SRL------------------------------------------------------------------------------------------------------
 srl <- read.csv("raw data/SRLmass.csv")
   srl$ID <- paste(srl$plot, srl$pot, sep = "-")
   srl <- merge(srl, plotsumm[3:4], all=TRUE)
   srl <- subset(srl, !is.na(volume))
-  #srl$SRL <- with(srl, (total_length_cm/100)*(srl_fw*(ss_dw/ss_fw)))
+  srl$SRL <- with(srl, (total_length_cm/100)*(srl_fw*(ss_dw/ss_fw)))
   srl$SRL2 <- with(srl, (total_length_cm/100)/(srl_fw*(ss_dw/ss_fw)))
   srl$volume <- as.factor(srl$volume)
   row.names(srl) <- NULL
   #srl <- vollab_func(srl)
   
-srl_clean <- srl[srl$SRL2 <= 100,]  
+##some values really high, remove to pots from boxplot of data  
+#srl_clean <- srl[srl$SRL2 <= 100,]  
+srl_clean2 <- srl[srl$ID != "5-8" & srl$ID != "7-8",]  
 
 
 ##srl_agg for paper table
-srl_agg <- summaryBy(SRL2 ~ volume, data=srl_clean, FUN=c(mean, se))
+srl_agg <- summaryBy(SRL2 ~ volume, data=srl, FUN=c(mean, se))
 write.csv(srl_agg, "calculated data/srl_means.csv", row.names=FALSE)
 
 ##Stats for sla and srl--------------------------------------------------------------------------------------
@@ -44,10 +29,11 @@ require(nlme)
 require(visreg)
 library(multcomp)
 
-srl_clean$volume <-  relevel(srl_clean$volume, ref="1000")
+srl_clean2$volume <-  relevel(srl_clean2$volume, ref="1000")
+srl$volume <-  relevel(srl$volume, ref="1000")
 
 #srl (not different)
-srl_container <- lme(SRL2 ~ volume, random= ~1|ID, data=srl_clean)
+srl_container <- lme(SRL2 ~ volume, random= ~1|ID, data=srl)
   anova(srl_container)
   summary(srl_container)
   visreg(srl_container)
@@ -57,23 +43,40 @@ srl_container <- lme(SRL2 ~ volume, random= ~1|ID, data=srl_clean)
   srl_siglets2 <- srl_siglets$mcletters$Letters
   write.csv(srl_siglets2, "master_scripts/sigletters/sigletts_plant/sl_srl.csv", row.names=FALSE)  
   
-  
 
+  
+##SLA[tncfree]------------------------------------------------------------------------------------------
+  
+sla_tnc <- read.csv("calculated data/TNC_content")
+  sla_tnc$volume <- as.factor(sla_tnc$volume) 
+  sla_tnc$Date <- as.Date(sla_tnc$Date)
+  sla_tnc$sla <- with(sla_tnc, area/mass)
+  sla_tnc$lma <- with(sla_tnc, mass/area)
+  
+  leaf_TNCfree <- subset(sla_tnc, select = c("ID", "volume", "sla_free", "lma_free","sla", "lma","Date"))
+  volorder<-order(leaf_TNCfree$volume, by=leaf_TNCfree$Date)
+  leaf_TNCfree <- leaf_TNCfree[volorder,]
+  
+#treatment means
+sla_tnc_campaign <- summaryBy(sla_free+lma_free+ sla+ lma ~ volume+Date, data=leaf_TNCfree, FUN=c(mean, se))
+#treatment means
+sla_tnc_agg <- summaryBy(sla_free+lma_free+ sla+ lma~volume, data=leaf_TNCfree, FUN=c(mean, se))
+  
 #sla
 sla_lm <- lme(sla_free ~ volume, random= ~1|ID, data=sla_tnc)
-  anova(sla_lm)
-  summary(sla_lm)
+anova(sla_lm)
+summary(sla_lm)
 
-  tukey_sla<- glht(sla_lm, linfct = mcp(volume = "Tukey"))
-  siglets_sla <- cld(tukey_sla)
-  visreg(sla_lm)
+tukey_sla<- glht(sla_lm, linfct = mcp(volume = "Tukey"))
+siglets_sla <- cld(tukey_sla)
+visreg(sla_lm)
 
 #when did sla free happen?
 sla_lm2 <- lme(sla_free ~ volume, random= ~1|ID, data=sla_tnc, subset=Date=="2013-03-07")
-  anova(sla_lm2)
-  visreg(sla_lm2)
-  tukey_srl2<- glht(sla_lm2, linfct = mcp(volume = "Tukey"))
-  cld(tukey_srl2)
+anova(sla_lm2)
+visreg(sla_lm2)
+tukey_srl2<- glht(sla_lm2, linfct = mcp(volume = "Tukey"))
+cld(tukey_srl2)
 
 
 #2panel plot of leaf and root morphology------------------------------------------------------------------
