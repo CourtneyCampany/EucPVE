edd74caf132b439489ac30d7218972022f2cdedd
Courtney Campany <courtneycampany@gmail.com>
2015-12-07 12:50:16 +1100

    moved figures and tables in text
    started a notes scripts
    
    made a backup of table and figures for pubs

---
+++ reproducibility_notes.R
@@ -0,0 +1,10 @@
+###reproducibility notes
+
+
+###between thesis and pub
+
+#1. I saved the figures and tables in a secondary RMD, maybe easier to convert back from thesis (which are inbedded)
+#2. Figure #s in thesis inbedded have been re# with thesis chapter Figure #.#
+#3. Likely will need to use pdf figures anyway, but then a list of figure tables is available.
+
+#4. for RMD will use two dummy docx files for thesis and pub, thesis will need to reflect binding requirements.
