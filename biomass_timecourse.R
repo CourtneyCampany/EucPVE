98c02ad15548e92dffae5f2df19023cb25a730eb
Courtney Campany <courtneycampany@gmail.com>
2016-03-09 11:32:36 +1100

    timecourse fig now has leafarea

--- biomass_timecourse.R
+++ biomass_timecourse.R
@@ -9,29 +9,51 @@ uniqueDate <- seq.Date(from=as.Date("2013/01/22"), to=as.Date("2013/05/21"), by=
 
 biomass_time$Date <- rep(uniqueDate, 7)
 
+#quick biomass plot
 plot(biomass~ Date, type='n', ylim=c(0, 200),data=biomass_time)
 points(biomass~ Date, data=biomass_time, col=cols[volume], pch=pchs[volume])
 
+#quick LA plot
+plot(leafarea~ Date, type='n', ylim=c(0, .55),data=biomass_time)
+points(leafarea~ Date, data=biomass_time, col=cols[volume], pch=pchs[volume])
 
 ##need to determine relative rate of decrease to free----------------------------------------------------------------------
 
-
 biomass_ls <- split(biomass_time, biomass_time$volume)
-free <- biomass_ls[[7]]$biomass
+free_mass <- biomass_ls[[7]]$biomass
+free_la <- biomass_ls[[7]]$leafarea
 
-biomass_reductions <- lapply(biomass_ls, function(x) {x$reduction_scaled = x$biomass/free; return(x)})
+
+biomass_reductions <- lapply(biomass_ls, function(x) {x$reduction_scaled = x$biomass/free_mass; return(x)})
+leafarea_reductions <- lapply(biomass_ls, function(x) {x$reduction_scaled = x$leafarea/free_la; return(x)})
+
+###make 2-panel with leaf area and biomass-------------------------------------------------------------------------
 
 ###multiply biomass *.5 if C
-windows(7,7)
+windows(7,10)
 
-par(mar=c(5,5,2,2), cex.axis=1, cex.lab = 1, las=1)
-plot(reduction_scaled ~ Date, pch=16, data= biomass_reductions[[1]], type='n',
-     xlab="",ylab="Daily Biomass Reduction Scaled to Free Seedlings")
+####multipanel plot of s1, s2
+par(las=1,mgp=c(3,1,0),mfrow=c(2,1), las=1)
+
+#mass
+par(mar=c(2,5,1,1))
+plot(reduction_scaled ~ Date, pch=16, data= biomass_reductions[[1]], type='n', ylim=c(0, 1.2),
+     xlab="",ylab="Biomass Reduction Scaled")
 for(i in 1:6){
   lines(biomass_reductions[[i]]$reduction_scaled~ biomass_reductions[[i]]$Date,  col=cols[i], lwd=2)
 }
 abline(1,0, lwd=2, lty=2, col=cols[7])
 legend("bottomleft", leglab, lty= c(rep(1,6),2),text.font=1, inset=0.01, title=vollab, col=palette(), bty='n',cex=1.0, lwd=1.5)
 
-dev.copy2pdf(file= "master_scripts/manuscript_figs/massreductions.pdf")  
+#leafarea
+par(mar=c(4,5,1,1))
+plot(reduction_scaled ~ Date, pch=16, data= leafarea_reductions[[1]], type='n',ylim=c(0, 1.2),
+     xlab="",ylab="Leaf Area Reduction Scaled")
+for(i in 1:6){
+  lines(leafarea_reductions[[i]]$reduction_scaled~ leafarea_reductions[[i]]$Date,  col=cols[i], lwd=2)
+}
+abline(1,0, lwd=2, lty=2, col=cols[7])
+
+
+dev.copy2pdf(file= "master_scripts/manuscript_figs/modeltimecourse.pdf")  
 dev.off() 
