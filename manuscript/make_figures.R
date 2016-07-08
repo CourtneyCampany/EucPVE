45c9d8ea6f87902228a7c487b5d867b346524410
Courtney Campany <courtneycampany@gmail.com>
2016-07-08 16:54:40 +1000

    made thesis folder in manuscripts
    added make figures script

---
+++ manuscript/make_figures.R
@@ -0,0 +1,22 @@
+to_pdf <- function(expr, filename, ..., verbose=TRUE) {
+  if(!file.exists(dirname(filename)))
+    dir.create(dirname(filename), recursive=TRUE)
+  if ( verbose )
+    cat(sprintf("Creating %s\n", filename))
+  pdf(filename, ...)
+  on.exit(dev.off())
+  eval.parent(substitute(expr))
+}
+
+
+source("functions and packages/startscripts.R")
+
+
+fn <- function(...)file.path("manuscript",...)
+
+to_pdf(source("master_scripts/allometryplotting.R"), fn("Figure1.pdf"), width=6, height=7.8)
+to_pdf(source("master_scripts/airvars_plotting.R"), fn("Figure2.pdf"), width=6, height=8)
+to_pdf(source("master_scripts/allocation_plotting.R"), fn("Figure3.pdf"), width=6, height=8)
+to_pdf(source("master_scripts/A_stats_plotting.R"), fn("Figure4.pdf"), width=6, height=6)
+to_pdf(source("master_scripts/Amax_TNC_N_plotting2.R"), fn("Figure5.pdf"), width=8, height=6)
+to_pdf(source("master_scripts/massmodel_optimize.R"), fn("Figure6.pdf"), width=6, height=8)
