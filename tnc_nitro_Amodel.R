ffae88a141d40136c5a42acd3e02ab626e8d3bf0
Courtney Campany <courtneycampany@gmail.com>
2017-01-09 11:39:19 +1100

    new script to compare R2 for A-tnc-N

--- tnc_nitro_Amodel.R
+++ tnc_nitro_Amodel.R
@@ -12,9 +12,7 @@ photo_chem<- vollab_func(photo_chem)
 photo_chem$starch_perc <- photo_chem$starch*100
 photo_chem$nitro_mg <- photo_chem$Nmass_notnc *1000
 
-#plot predicted and observed in bins------------------------------------------------------
 
-#make bin levels and mid points for both starch and nitrogen
 #model and stats
 Afit_full <- lmer(A_mass ~ nitro_mg+starch_perc+nitro_mg:starch_perc + (1|ID), data=photo_chem)
 #####model parameters for plotting
@@ -22,3 +20,20 @@ f <- fixef(Afit_full)
 coef(Afit_full)
 
 anova(Afit_full)
+summary(Afit_full)
+
+
+#nitro only
+Afit_nitro <- lmer(A_mass ~ nitro_mg + (1|ID), data=photo_chem)
+coef(Afit_nitro)
+anova(Afit_nitro)
+summary(Afit_nitro)
+
+Afit_tnc <- lmer(A_mass ~ starch_perc + (1|ID), data=photo_chem)
+coef(Afit_tnc)
+anova(Afit_tnc)
+summary(Afit_tnc)
+
+##compare R2 values to test contribution of each vs whole model
+
+
